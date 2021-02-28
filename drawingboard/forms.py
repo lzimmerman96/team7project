2e9592b48bb74bd608009b979c9877701f7e61e8
lzimmerman96 <77468510+lzimmerman96@users.noreply.github.com>
2021-02-28 15:19:28 -0600

    added log-in and delete functions

--- drawingboard/forms.py
+++ drawingboard/forms.py
@@ -1,1 +1,8 @@
 from django import forms
+from django.contrib.auth.models import Artist
+
+
+class ArtistDeleteForm(forms.ModelForm):
+    class Meta:
+        model = Artist
+        fields = []   #Form has only submit button.  Empty "fields" list still necessary, though.
--- drawingboard/settings.py
+++ drawingboard/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'drawingboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'gallery/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
