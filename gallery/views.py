8d1059e12918630cb790728306557c340cc4f8b5
meetmanjusri <meetmanjusri@gmail.com>
2021-03-03 23:04:54 -0600

    Created artist edit,delete,new html

--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import *
 from .forms import *
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+
+
 
 now = timezone.now()
 
@@ -10,6 +14,49 @@ def home(request):
     return render(request, 'gallery/home.html',
                   {'gallery': home})
 
+
+def artist_list(request):
+    artist = Artist.objects.all()
+    return render(request, 'gallery/artist_list.html',
+                 {'artists': artist})
+
+
+def artist_new(request):
+   if request.method == "POST":
+       form = ArtistForm(request.POST)
+       if form.is_valid():
+           artist = form.save(commit=False)
+           artist.created_date = timezone.now()
+           artist.save()
+           artists = Artist.objects.all()
+           return render(request, 'gallery/artist_list.html',
+                         {'artists': artists})
+   else:
+       form = ArtistForm()
+       # print("Else")
+   return render(request, 'gallery/artist_new.html', {'form': form})
+
+def artist_edit(request, pk):
+   artist = get_object_or_404(Artist, pk=pk)
+   if request.method == "POST":
+       # update
+       form = ArtistForm(request.POST, instance=artist)
+       if form.is_valid():
+           artist = form.save(commit=False)
+           artist.updated_date = timezone.now()
+           artist.save()
+           artist = Artist.objects.all()
+           return render(request, 'gallery/artist_list.html',
+                         {'artists': artist})
+   else:
+        # edit
+       form = ArtistForm(instance=artist)
+   return render(request, 'gallery/artist_edit.html', {'form': form})
+
+def artist_delete(request, pk):
+   artist = get_object_or_404(Artist, pk=pk)
+   artist.delete()
+   return redirect('gallery:artist_list')
 
 def artwork_list(request):
     artwork = Artwork.objects.all()
