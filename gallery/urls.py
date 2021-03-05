cc4bf99bd10ee66b35d64182520a4156ae8d9dd6
kbookers <77553411+kbookers@users.noreply.github.com>
2021-03-04 18:00:36 -0600

    Added features to create & update accounts
    Have not tested update account feature yet, need login feature running first

--- gallery/urls.py
+++ gallery/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('artwork_list', views.artwork_list, name='favorite_list'),
     path('artwork_list', views.artwork_list, name='rating_list'),
     path('artwork_list', views.artwork_list, name='tag_list'),
+    path('create_account', views.create_account, name='create_account'),
+    path('account/<int:pk>/edit/', views.update_account_details, name='update_account_details'),
+    # path('update_account_details', views.update_account_details, name='update_account_details')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -3,8 +3,8 @@ from .models import *
 from .forms import *
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
-
 
 
 now = timezone.now()
@@ -15,48 +15,78 @@ def home(request):
                   {'gallery': home})
 
 
+def create_account(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('gallery:home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/create_account.html', {'form': form})
+
+
+def update_account_details(request, pk):
+    account = get_object_or_404(Artist, pk=pk)
+    if request.method == "POST":
+        # update
+        form = ArtistForm(request.POST, instance=account)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.updated_date = timezone.now()
+            account.save()
+            return redirect('gallery/home.html')
+    else:
+        # edit
+        form = ArtistForm(instance=account)
+    return render(request, 'gallery/home.html', {'form': form})
+
+
 def artist_list(request):
     artist = Artist.objects.all()
     return render(request, 'gallery/artist_list.html',
-                 {'artists': artist})
+                  {'artists': artist})
 
 
 def artist_new(request):
-   if request.method == "POST":
-       form = ArtistForm(request.POST)
-       if form.is_valid():
-           artist = form.save(commit=False)
-           artist.created_date = timezone.now()
-           artist.save()
-           artists = Artist.objects.all()
-           return render(request, 'gallery/artist_list.html',
+    if request.method == "POST":
+        form = ArtistForm(request.POST)
+        if form.is_valid():
+            artist = form.save(commit=False)
+            artist.created_date = timezone.now()
+            artist.save()
+            artists = Artist.objects.all()
+            return render(request, 'gallery/artist_list.html',
                          {'artists': artists})
-   else:
-       form = ArtistForm()
-       # print("Else")
-   return render(request, 'gallery/artist_new.html', {'form': form})
+    else:
+        form = ArtistForm()
+        # print("Else")
+    return render(request, 'gallery/artist_new.html', {'form': form})
+
 
 def artist_edit(request, pk):
-   artist = get_object_or_404(Artist, pk=pk)
-   if request.method == "POST":
-       # update
-       form = ArtistForm(request.POST, instance=artist)
-       if form.is_valid():
-           artist = form.save(commit=False)
-           artist.updated_date = timezone.now()
-           artist.save()
-           artist = Artist.objects.all()
-           return render(request, 'gallery/artist_list.html',
+    artist = get_object_or_404(Artist, pk=pk)
+    if request.method == "POST":
+        # update
+        form = ArtistForm(request.POST, instance=artist)
+        if form.is_valid():
+            artist = form.save(commit=False)
+            artist.updated_date = timezone.now()
+            artist.save()
+            artist = Artist.objects.all()
+            return render(request, 'gallery/artist_list.html',
                          {'artists': artist})
-   else:
+    else:
         # edit
-       form = ArtistForm(instance=artist)
-   return render(request, 'gallery/artist_edit.html', {'form': form})
+        form = ArtistForm(instance=artist)
+    return render(request, 'gallery/artist_edit.html', {'form': form})
+
 
 def artist_delete(request, pk):
-   artist = get_object_or_404(Artist, pk=pk)
-   artist.delete()
-   return redirect('gallery:artist_list')
+    artist = get_object_or_404(Artist, pk=pk)
+    artist.delete()
+    return redirect('gallery:artist_list')
+
 
 def artwork_list(request):
     artwork = Artwork.objects.all()
