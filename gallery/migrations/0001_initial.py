849413c5e1a7d01946ab8ce0117113f5197da172
lzimmerman96 <77468510+lzimmerman96@users.noreply.github.com>
2021-04-08 19:37:32 -0500

    test
    for maya

--- gallery/migrations/0001_initial.py
+++ gallery/migrations/0001_initial.py
@@ -4,7 +4,7 @@ from django.conf import settings
 import django.core.validators
 from django.db import migrations, models
 import django.db.models.deletion
-
+# test comment
 
 class Migration(migrations.Migration):
 
